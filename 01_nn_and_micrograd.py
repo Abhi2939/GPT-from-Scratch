91c5d4705acc695fd4755efcb15e60bd0582fe25
Abhi2939 <kumarabhinandan2000@gmail.com>
2026-07-11 04:59:32 +0530

    Basics of NN

--- 01_nn_and_micrograd.py
+++ 01_nn_and_micrograd.py
@@ -10,6 +10,7 @@ Original file is located at
 import math
 import numpy as np
 import matplotlib.pyplot as plt
+import random
 
 def f(x):
   return 3*x**2 - 4*x + 5
@@ -49,6 +50,7 @@ class Value:
     self.data = data
     self.grad = 0.0
     self._prev = set(_children)
+    self._backward = lambda: None
     self._op = _op
     self.label = label
 
@@ -56,12 +58,88 @@ class Value:
     return f"Value(data={self.data})"
 
   def __add__(self,other):
+    other = other if isinstance(other, Value) else Value(other)
     out = Value(self.data + other.data,(self,other),'+')
+
+    def _backward():
+      self.grad += 1.0 * out.grad
+      other.grad += 1.0 * out.grad
+    out._backward = _backward
+
     return out
 
   def __mul__(self,other):
+    other = other if isinstance(other, Value) else Value(other)
     out = Value(self.data * other.data,(self,other),'*')
+
+    def _backward():
+      self.grad += other.data * out.grad
+      other.grad += self.data * out.grad
+    out._backward = _backward
     return out
+
+  def __pow__(self, other):
+    assert isinstance(other, (int, float)), "only supporting int/float powers for now"
+    out = Value(self.data**other, (self,), f'**{other}')
+
+    def _backward():
+        self.grad += other * (self.data ** (other - 1)) * out.grad
+    out._backward = _backward
+
+    return out
+
+  def __rmul__(self, other): # other * self
+    return self * other
+
+  def __truediv__(self, other): # self / other
+    return self * other**-1
+
+  def __neg__(self): # -self
+    return self * -1
+
+  def __sub__(self, other): # self - other
+    return self + (-other)
+
+  def __radd__(self, other): # other + self
+    return self + other
+
+
+  def exp(self):
+    x = self.data
+    out = Value(math.exp(x), (self, ), 'exp')
+
+    def _backward():
+      self.grad += out.data * out.grad
+    out._backward = _backward
+
+    return out
+
+  def tanh(self):
+    x = self.data
+    t = (math.exp(2*x)-1)/(math.exp(2*x)+1)
+    out = Value(t,(self,),'tanh')
+
+    def _backward():
+      self.grad += (1-t**2)*out.grad
+    out._backward = _backward
+
+    return out
+
+  def backward(self):
+
+    topo = []
+    visited = set()
+    def build_topo(v):
+      if v not in visited:
+        visited.add(v)
+        for child in v._prev:
+          build_topo(child)
+        topo.append(v)
+    build_topo(self)
+
+    self.grad = 1.0
+    for node in reversed(topo):
+      node._backward()
 
 a = Value(2.0,label='a')
 b = Value(-3.0,label='b')
@@ -110,4 +188,204 @@ def draw_dot(root):
   return dot
 
 draw_dot(L)
+
+"""BackProp on NN"""
+
+def lol():
+
+  h = 0.001
+
+  a = Value(2.0, label='a')
+  b = Value(-3.0, label='b')
+  c = Value(10.0, label='c')
+  e = a*b; e.label = 'e'
+  d = e + c; d.label = 'd'
+  f = Value(-2.0, label='f')
+  L = d * f; L.label = 'L'
+  L1 = L.data
+
+  a = Value(2.0, label='a')
+  b = Value(-3.0, label='b')
+  b.data += h
+  c = Value(10.0, label='c')
+  e = a*b; e.label = 'e'
+  d = e + c; d.label = 'd'
+  f = Value(-2.0, label='f')
+  L = d * f; L.label = 'L'
+  L2 = L.data
+
+  print((L2 - L1)/h)
+
+lol()
+
+plt.plot(np.arange(-5,5,0.2) , np.tanh(np.arange(-5,5,0.2)));
+plt.grid()
+
+# inputs x1,x2
+x1 = Value(2.0, label='x1')
+x2 = Value(0.0, label='x2')
+
+# weights w1,w2
+w1 = Value(-3.0, label='w1')
+w2 = Value(1.0, label='w2')
+
+# bias of the neuron
+b = Value(6.8813735870195432, label='b')
+
+# x1*w1 + x2*w2 + b
+x1w1 = x1*w1; x1w1.label = 'x1*w1'
+x2w2 = x2*w2; x2w2.label = 'x2*w2'
+x1w1x2w2 = x1w1 + x2w2; x1w1x2w2.label = 'x1*w1 + x2*w2'
+n = x1w1x2w2 + b; n.label = 'n'
+o = n.tanh(); o.label = 'o'
+
+draw_dot(o)
+
+o.grad = 1.0
+o.backward()
+
+topo = []
+visited = set()
+def build_topo(v):
+  if v not in visited:
+    visited.add(v)
+    for child in v._prev:
+      build_topo(child)
+    topo.append(v)
+build_topo(o)
+topo
+
+n._backward()
+
+# inputs x1,x2
+x1 = Value(2.0, label='x1')
+x2 = Value(0.0, label='x2')
+# weights w1,w2
+w1 = Value(-3.0, label='w1')
+w2 = Value(1.0, label='w2')
+# bias of the neuron
+b = Value(6.8813735870195432, label='b')
+# x1*w1 + x2*w2 + b
+x1w1 = x1*w1; x1w1.label = 'x1*w1'
+x2w2 = x2*w2; x2w2.label = 'x2*w2'
+x1w1x2w2 = x1w1 + x2w2; x1w1x2w2.label = 'x1*w1 + x2*w2'
+n = x1w1x2w2 + b; n.label = 'n'
+o = n.tanh(); o.label = 'o'
+o.backward()
+
+draw_dot(o)
+
+# inputs x1,x2
+x1 = Value(2.0, label='x1')
+x2 = Value(0.0, label='x2')
+# weights w1,w2
+w1 = Value(-3.0, label='w1')
+w2 = Value(1.0, label='w2')
+# bias of the neuron
+b = Value(6.8813735870195432, label='b')
+# x1*w1 + x2*w2 + b
+x1w1 = x1*w1; x1w1.label = 'x1*w1'
+x2w2 = x2*w2; x2w2.label = 'x2*w2'
+x1w1x2w2 = x1w1 + x2w2; x1w1x2w2.label = 'x1*w1 + x2*w2'
+n = x1w1x2w2 + b; n.label = 'n'
+# ----
+e = (2*n).exp()
+o = (e - 1) / (e + 1)
+# ----
+o.label = 'o'
+o.backward()
+draw_dot(o)
+
+"""#Doing same thing in PyTorch"""
+
+import torch
+
+x1 = torch.Tensor([2.0]).double()                ; x1.requires_grad = True
+x2 = torch.Tensor([0.0]).double()                ; x2.requires_grad = True
+w1 = torch.Tensor([-3.0]).double()               ; w1.requires_grad = True
+w2 = torch.Tensor([1.0]).double()                ; w2.requires_grad = True
+b = torch.Tensor([6.8813735870195432]).double()  ; b.requires_grad = True
+n = x1*w1 + x2*w2 + b
+o = torch.tanh(n)
+
+print(o.data.item())
+o.backward()
+
+print('---')
+print('x2', x2.grad.item())
+print('w2', w2.grad.item())
+print('x1', x1.grad.item())
+print('w1', w1.grad.item())
+
+class Neuron:
+
+    def __init__(self, nin):
+      self.w = [Value(random.uniform(-1,1)) for _ in range(nin)]
+      self.b = Value(random.uniform(-1,1))
+
+    def __call__(self,x):
+
+      act = sum((wi*xi for wi,xi in zip(self.w,x)),self.b)
+      out = act.tanh()
+      return out
+
+    def parameters(self):
+      return self.w + [self.b]
+
+class Layer:
+
+  def __init__(self, nin, nout):
+    self.neurons = [Neuron(nin) for _ in range(nout)]
+
+  def __call__(self, x):
+    outs = [n(x) for n in self.neurons]
+    return outs[0] if len(outs) == 1 else outs
+
+  def parameters(self):
+    return [p for neuron in self.neurons for p in neuron.parameters()]
+
+class MLP:
+
+  def __init__(self, nin, nouts):
+    sz = [nin] + nouts
+    self.layers = [Layer(sz[i], sz[i+1]) for i in range(len(nouts))]
+
+  def __call__(self, x):
+    for layer in self.layers:
+      x = layer(x)
+    return x
+
+  def parameters(self):
+    return [p for layer in self.layers for p in layer.parameters()]
+
+x = [2.0,3.0,-1.0]
+n = MLP(3,[4,4,1])
+n(x)
+
+draw_dot(n(x))
+
+xs = [
+  [2.0, 3.0, -1.0],
+  [3.0, -1.0, 0.5],
+  [0.5, 1.0, 1.0],
+  [1.0, 1.0, -1.0],
+]
+ys = [1.0, -1.0, -1.0, 1.0]
+
+for k in range(20):
+
+  # forward pass
+  ypred = [n(x) for x in xs]
+  loss = sum((yout - ygt)**2 for ygt, yout in zip(ys, ypred))
+
+  # backward pass
+  for p in n.parameters():
+    p.grad = 0.0
+  loss.backward()
+
+  # update
+  for p in n.parameters():
+    p.data += -0.1 * p.grad
+
+  print(k, loss.data)
 
