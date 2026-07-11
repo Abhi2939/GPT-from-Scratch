7eea70bb260dacac3023bf30529ae666026988c3
Abhi2939 <kumarabhinandan2000@gmail.com>
2026-07-12 04:45:14 +0530

    Bigrams

--- 02_makeover_bigrams.py
+++ 02_makeover_bigrams.py
@@ -57,3 +57,205 @@ for i in range(27):
         plt.text(j, i, N[i, j].item(), ha="center", va="top", color='gray')
 plt.axis('off');
 
+N[0, :].shape
+
+p = N[0].float()
+
+g = torch.Generator().manual_seed(2147483647)
+ix = torch.multinomial(p, num_samples=1, replacement=True, generator=g).item()
+itos[ix]
+
+g = torch.Generator().manual_seed(2147483647)
+p = torch.rand(3, generator=g)
+p = p / p.sum()
+p
+
+torch.multinomial(p, num_samples=100, replacement=True, generator=g)
+
+p.shape
+
+P.sum(1,keepdim=True).shape
+
+P = (N+1).float()
+P /= P.sum(1,keepdims=True)
+
+g = torch.Generator().manual_seed(2147483647)
+
+for i in range(5):
+  out = []
+  ix = 0
+  while True:
+
+    p = P[ix]
+
+    ix = torch.multinomial(p, num_samples=1, replacement=True, generator=g).item()
+    out.append(itos[ix])
+    if ix == 0:
+      break
+  print(''.join(out))
+
+log_likelihood = 0.0
+n = 0
+
+for w in words:
+#for w in ["andrejq"]:
+  chs = ['.'] + list(w) + ['.']
+  for ch1, ch2 in zip(chs, chs[1:]):
+    ix1 = stoi[ch1]
+    ix2 = stoi[ch2]
+    prob = P[ix1, ix2]
+    logprob = torch.log(prob)
+    log_likelihood += logprob
+    n += 1
+    #print(f'{ch1}{ch2}: {prob:.4f} {logprob:.4f}')
+
+print(f'{log_likelihood=}')
+nll = -log_likelihood
+print(f'{nll=}')
+print(f'{nll/n}')
+
+"""#Neural Network Approach"""
+
+xs,ys = [] , []
+
+for w in words[:1]:
+  chs = ['.'] + list(w) + ['.']
+  for ch1,ch2 in zip(chs,chs[1:]):
+    ix1 = stoi[ch1]
+    ix2 = stoi[ch2]
+    print(ch1,ch2)
+    xs.append(ix1)
+    ys.append(ix2)
+
+xs = torch.tensor(xs)
+ys = torch.tensor(ys)
+
+xs
+
+ys
+
+import torch.nn.functional as F
+xenc = F.one_hot(xs,num_classes=27).float()
+xenc
+
+xenc.shape
+
+plt.imshow(xenc)
+
+W = torch.randn((27,1))
+xenc @ W
+
+logits = xenc @ W
+counts = logits.exp()
+probs = counts / counts.sum(1,keepdims=True)
+probs
+
+probs[0]
+
+probs[0].shape
+
+probs[0].sum()
+
+"""Smaller Version"""
+
+xs
+
+ys
+
+# randomly initialize 27 neurons' weights. each neuron receives 27 inputs
+g = torch.Generator().manual_seed(2147483647)
+W = torch.randn((27, 27), generator=g)
+
+# Forward Pass
+xenc = F.one_hot(xs, num_classes=27).float() # input to the network: one-hot encoding
+logits = xenc @ W # predict log-counts
+
+counts = logits.exp() # counts, equivalent to N
+probs = counts / counts.sum(1, keepdims=True) # probabilities for next character
+# btw: the last 2 lines here are together called a 'softmax'
+
+probs.shape
+
+nlls = torch.zeros(5)
+for i in range(5):
+  # i-th bigram:
+  x = xs[i].item() # input character index
+  y = ys[i].item() # label character index
+  print('--------')
+  print(f'bigram example {i+1}: {itos[x]}{itos[y]} (indexes {x},{y})')
+  print('input to the neural net:', x)
+  print('output probabilities from the neural net:', probs[i])
+  print('label (actual next character):', y)
+  p = probs[i, y]
+  print('probability assigned by the net to the the correct character:', p.item())
+  logp = torch.log(p)
+  print('log likelihood:', logp.item())
+  nll = -logp
+  print('negative log likelihood:', nll.item())
+  nlls[i] = nll
+
+print('=========')
+print('average negative log likelihood, i.e. loss =', nlls.mean().item())
+
+xs
+
+ys
+
+# randomly initialize 27 neurons' weights. each neuron receives 27 inputs
+g = torch.Generator().manual_seed(2147483647)
+W = torch.randn((27, 27), generator=g,requires_grad=True)
+
+# Forward Pass
+xenc = F.one_hot(xs, num_classes=27).float()
+logits = xenc @ W
+counts = logits.exp()
+probs = counts / counts.sum(1, keepdims=True)
+loss = -probs[torch.arange(5),ys].log().mean()
+
+print(loss.item())
+
+# Backward Pass
+W.grad = None
+loss.backward()
+
+#update
+W.data += -0.1 * W.grad
+
+"""Optimization"""
+
+# create the dataset
+xs, ys = [], []
+for w in words:
+  chs = ['.'] + list(w) + ['.']
+  for ch1, ch2 in zip(chs, chs[1:]):
+    ix1 = stoi[ch1]
+    ix2 = stoi[ch2]
+    xs.append(ix1)
+    ys.append(ix2)
+xs = torch.tensor(xs)
+ys = torch.tensor(ys)
+num = xs.nelement()
+print('number of examples: ', num)
+
+# initialize the 'network'
+g = torch.Generator().manual_seed(2147483647)
+W = torch.randn((27, 27), generator=g, requires_grad=True)
+
+# gradient descent
+for k in range(1):
+
+  # forward pass
+  xenc = F.one_hot(xs, num_classes=27).float() # input to the network: one-hot encoding
+  logits = xenc @ W # predict log-counts
+  counts = logits.exp() # counts, equivalent to N
+  probs = counts / counts.sum(1, keepdims=True) # probabilities for next character
+  loss = -probs[torch.arange(num), ys].log().mean() + 0.01*(W**2).mean()
+  print(loss.item())
+
+  # backward pass
+  W.grad = None # set to zero the gradient
+  loss.backward()
+
+  # update
+  W.data += -50 * W.grad
+
